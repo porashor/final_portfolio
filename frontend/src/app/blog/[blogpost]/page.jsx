4f31207a53d096867c4a6fe44bb18f67c37ddaaf
Parashar Das <87603143+porashor@users.noreply.github.com>
2025-11-04 15:09:29 +0600

    blog dynamic page create

---
+++ frontend/src/app/blog/[blogpost]/page.jsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { formatDistanceToNow } from "date-fns";
+import ReactMarkdown from 'react-markdown';
+const page = async ({ params }) => {
+  const { blogpost } = await params;
+  let data;
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/blog/${blogpost}`
+    );
+    data = await response.json();
+} catch (error) {
+    console.log(error);
+}
+const timeAgo = formatDistanceToNow(new Date(data.createdAt), { addSuffix: true });
+  return (
+    <div className="w-full">
+      <div className="py-5 lg:py-10 w-[90%] mx-auto">
+        <a href="/">Home</a> {">"} <a href="/blog">Blog</a> {">"} {data.title}
+      </div>
+      <hr />
+      <div className="w-full flex items-center justify-center p-5 lg:p-10 ">
+        <div className="relative w-[200px] md:w-[300px] lg:w-[400px] aspect-square p-5">
+          <Image
+            src={data.image}
+            alt="My profile picture"
+            className="object-cover"
+            fill
+          />
+        </div>
+      </div>
+      <div className="w-[90%] py-5 lg:py-10 mx-auto">
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">{data.title}</h1>
+        <p>autor : {data.autor}</p>
+        <p>Posted on : {timeAgo}</p>
+      </div>
+      <div className="w-[90%] mx-auto text-lg lg:text-xl leading-11 pb-5 lg:pb-10">
+        <ReactMarkdown>{data.description}</ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -2,7 +2,6 @@ import { formatDistanceToNow } from "date-fns";
 import Image from 'next/image'
 const BlogCard = ({blog}) => {
   const timeAgo = formatDistanceToNow(new Date(blog.createdAt), { addSuffix: true });
-  console.log(timeAgo)
   return (
     <div className='w-full bg-white min-h-[150px] rounded logo-light grid md:grid-cols-[1fr_3fr] gap-5 lowercase'>
         <div className='w-[200px] aspect-square object-cover hidden md:block relative'>
@@ -13,7 +12,7 @@ const BlogCard = ({blog}) => {
             <h1 className='text-sm font-thin'><span className='font-bold'>Author:</span> {blog.autor}</h1>
             <p className='text-sm font-thin line-clamp-2'>{blog.description}</p>
             <div className='flex items-center justify-between'>
-                <button className='muted-rose logo-dark text-xs py-1 px-2 font-thin'>read more</button>
+              <a href={"blog/"+blog._id}className='muted-rose logo-dark text-xs py-1 px-2 font-thin'>read more</a>
                 <p className='font-thin text-xs'>{timeAgo}</p>
             </div>
         </div>
